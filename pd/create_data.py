46a4454d59f7f3f3668ff636c94599255d738ee5
koma2-hub <komatsudlam@gmail.com>
2024-12-04 17:22:10 +0900

    a

--- pd/create_data.py
+++ pd/create_data.py
@@ -1,9 +1,17 @@
 import pandas as pd
 
-
+#dataframeはディクショナリと同じ形、ラベルはリストと同じ形になる。
 pd.DataFrame({'yes': [50, 21], 'no':[131, 2]})
 
 pd.DataFrame({'Bob': ['I liked it.', 'It was awful.'], 
               'Sue': ['Pretty good.', 'Bland.']}, 
               index = ['Product A', 'Product B'] ) 
+
+data = pd.Series([0,1,2], index = ["a","b","c"], name = "test data")
+#print(data)
+
+#index_col インデックスが始める値？
+reviews = pd.read_csv("winemag-data-130k-v2.csv", index_col = 1)
+print(reviews)
+#dataframeはseriesの寄せ集めと考えられる。
 
--- pd/manipulate.py
+++ pd/manipulate.py
@@ -1,5 +1,25 @@
 import pandas as pd
 
-reviews = pd.read_sql("winemag-data-130k-v2.csv")
+reviews = pd.read_csv("winemag-data-130k-v2.csv",index_col = 0)
 
-print(reviews)
+#print(reviews)
+
+#loc, iloc,indexの振る舞いが違う
+
+#全ての行の0列目を出力する。
+reviews_country = reviews.iloc[:,0]
+#print(reviews_country)
+
+#ilocではできない操作
+reviews_country = reviews.loc[:,'country']
+
+#listを引数として渡すこともできる
+reviews.loc[:, ['taster_name', 'taster_twitter_handle', 'points']]
+
+#表から国がItalyのものだけを抽出する
+reviews_Italy = reviews.loc[reviews.country == 'Italy']
+print(reviews_Italy)
+
+#条件分は複数かける
+reviews_Italy = reviews.loc[(reviews.country == 'Italy') & (reviews.points >= 90)]
+
