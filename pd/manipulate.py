c4f8ef27b123b447c25b4002b885a6d9730436f5
unknown <kobayakawajp2020@gmail.com>
2024-12-04 18:26:13 +0900

    yaa

--- pd/manipulate.py
+++ pd/manipulate.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+<<<<<<< HEAD
 reviews = pd.read_csv("winemag-data-130k-v2.csv", index_col = 0)
 pd.set_option('display.max_row', 5)
 
@@ -23,4 +24,28 @@ reviews.set_index("title")
 
 #表のcountryがItalyであるものを表示
 print(reviews.loc[reviews.country == 'Italy'])
+=======
+reviews = pd.read_csv("winemag-data-130k-v2.csv",index_col = 0)
+
+#print(reviews)
+
+#loc, iloc,indexの振る舞いが違う
+
+#全ての行の0列目を出力する。
+reviews_country = reviews.iloc[:,0]
+#print(reviews_country)
+
+#ilocではできない操作
+reviews_country = reviews.loc[:,'country']
+
+#listを引数として渡すこともできる
+reviews.loc[:, ['taster_name', 'taster_twitter_handle', 'points']]
+
+#表から国がItalyのものだけを抽出する
+reviews_Italy = reviews.loc[reviews.country == 'Italy']
+print(reviews_Italy)
+
+#条件分は複数かける
+reviews_Italy = reviews.loc[(reviews.country == 'Italy') & (reviews.points >= 90)]
+>>>>>>> 46a4454d59f7f3f3668ff636c94599255d738ee5
 
