856d16f3c173bb6b89ac49e59f2955ea9737ab9f
unknown <kobayakawajp2020@gmail.com>
2024-12-04 18:52:50 +0900

    hi

--- pd/manipulate.py
+++ pd/manipulate.py
@@ -1,30 +1,6 @@
 import pandas as pd
 
-<<<<<<< HEAD
-reviews = pd.read_csv("winemag-data-130k-v2.csv", index_col = 0)
-pd.set_option('display.max_row', 5)
 
-print(reviews)
-#表の中のcoutryだけを表示
-print(reviews.country)
-print(reviews['country'])
-
-#表の０行目を表示
-print(reviews.iloc[0])
-#print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-#表における1~3行、0列目を表示
-print(reviews.iloc[:3,0])
-
-#表における後ろから5行の全部を表示
-print(reviews.iloc[-5:,:])
-
-#表にtitleを挿入
-reviews.set_index("title")
-#print(reviews)
-
-#表のcountryがItalyであるものを表示
-print(reviews.loc[reviews.country == 'Italy'])
-=======
 reviews = pd.read_csv("winemag-data-130k-v2.csv",index_col = 0)
 
 #print(reviews)
@@ -47,5 +23,5 @@ print(reviews_Italy)
 
 #条件分は複数かける
 reviews_Italy = reviews.loc[(reviews.country == 'Italy') & (reviews.points >= 90)]
->>>>>>> 46a4454d59f7f3f3668ff636c94599255d738ee5
+
 
